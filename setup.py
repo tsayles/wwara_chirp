6fb822c341c892c4b9a5f5e4ea6e8ced573783f9
tsayles <TSayles@Soot-n-Smoke.com>
2025-02-17 23:31:51 -0800

    Enhance setup.py with additional metadata and long description

--- setup.py
+++ setup.py
@@ -6,7 +6,32 @@ setup(
     packages=['wwara-csv-to-chirp-csv'],
     url='https://github.com/tsayles/wwara-chirp.git',
     license='MIT',
-    author='tsayles',
+    aauthor='Tom Sayles (KE4HET), GitHub Copilot',
     author_email='tsayles@Soot-n-Smoke.com',
-    description='WWARA CHIRP Export Script Update'
+    description='WWARA CHIRP Export Script Update',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Amateur Radio Operators',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    entry_points={
+        'console_scripts': [
+            'wwara-chirp=wwara_csv_to_chirp_csv:main',
+        ],
+    },
+    python_requires='>=3.6',
+    keywords='WWARA CHIRP',
+    project_urls={
+        'Bug Reports': 'https://github.com/tsayles/wwara-chirp/issues',
+        'Source': 'https://github.com/tsayles/wwara-chirp',
+    }
 )
