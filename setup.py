028d813940ff65686b1fdca6bd348efa86a88dca
tsayles <TSayles@Soot-n-Smoke.com>
2025-02-20 00:05:16 -0800

    Bump version: 2.0.1 → 2.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wwara-chirp',
-    version='2.0.1',
+    version='2.0.2',
     # packages=['wwara-chirp'],
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
